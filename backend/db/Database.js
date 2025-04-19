159ff4a755034aa3f77ae652abe396b12a242b2a
unknown <srimandgl2004@gmail.com>
2025-04-19 09:53:30 +0530

    done chages

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     mongoose
-        .connect(process.env.DB_URL) 
+        .connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }) 
         .then((data) => {
             console.log(`MongoDB connected with server: ${data.connection.host}`);
         })
